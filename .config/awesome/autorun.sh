6d64cebf9189cd947e66024447c14d7ffad65804
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2022-12-11 10:06:07 -0300

    Readded unclutter

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -3,10 +3,11 @@
 run() {
     if ! pgrep -f "$1" ;
     then
-        "$@" &
+        $@ &
     fi
 }
 
 run "mpd"
 run "picom"
 run "/usr/lib/xfce-polkit/xfce-polkit"
+run "unclutter --timeout 3 --jitter 5 --start-hidden"
