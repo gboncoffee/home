fa78b0ef9d7013164fea3a80db5f681aea2ee6b5
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-01-14 18:52:57 -0300

    Added useless gaps

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -18,7 +18,7 @@ local theme = {}
 
 theme.c = c
 
-theme.useless_gap       = nil
+theme.useless_gap       = 4
 theme.font              = "CaskaydiaCove Nerd Font 12"
 theme.notification_font = "CaskaydiaCove Nerd Font 14"
 theme.wallpaper         = os.getenv("HOME") .. "/.config/wallpaper"
