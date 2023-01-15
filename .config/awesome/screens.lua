91b2e917bdd009c08bea1603d47e4f51e8ae8bf6
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-01-15 01:30:38 -0300

    Added buttons

--- .config/awesome/screens.lua
+++ .config/awesome/screens.lua
@@ -8,16 +8,53 @@ M.set_wallpaper = function(s)
     gears.wallpaper.maximized(beautiful.wallpaper, s)
 end
 
+local taglist_buttons = gears.table.join(
+    awful.button({ }, 1, function(t) t:view_only() end),
+    awful.button({ modkey }, 1, function(t)
+        if client.focus then
+            client.focus:move_to_tag(t)
+        end
+    end),
+    awful.button({ }, 3, awful.tag.viewtoggle),
+    awful.button({ modkey }, 3, function(t)
+        if client.focus then
+            client.focus:toggle_tag(t)
+        end
+    end),
+    awful.button({ }, 4, function(t) awful.tag.viewnext(t.screen) end),
+    awful.button({ }, 5, function(t) awful.tag.viewprev(t.screen) end)
+)
+
 M.initscr = function(s)
 
     M.set_wallpaper(s)
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    awful.tag(
+        {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        },
+        s,
+        awful.layout.layouts[1])
 
     local mylayoutbox = awful.widget.layoutbox(s)
+    mylayoutbox:buttons(gears.table.join(
+        awful.button({ }, 1, function () awful.layout.inc( 1) end),
+        awful.button({ }, 3, function () awful.layout.inc(-1) end),
+        awful.button({ }, 4, function () awful.layout.inc( 1) end),
+        awful.button({ }, 5, function () awful.layout.inc(-1) end)
+    ))
 
     local mytaglist = awful.widget.taglist {
         filter  = awful.widget.taglist.filter.all,
-        screen  = s
+        screen  = s,
+        buttons = taglist_buttons,
     }
 
     require "widgets".set_bar(s, mytaglist, mylayoutbox)
