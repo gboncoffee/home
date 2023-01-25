48dabdbb0d0e13ea64fabb3209dc78ee90fc71a2
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-01-24 23:33:23 -0300

    Mouse follow focus was a TERRIBLE idea
    This reverts commit 77ec0526f5073bddf6ecd3f0c78a40adf1cb4381.

--- .config/awesome/signals.lua
+++ .config/awesome/signals.lua
@@ -1,22 +1,5 @@
 local awful     = require "awful"
 local beautiful = require "beautiful"
-local gears     = require "gears"
-
-local move_mouse_onto_focused_client = function()
-    local c = client.focus
-    gears.timer({ timeout = 0.1,
-        autostart   = true,
-        single_shot = true,
-        callback    = function()
-            if mouse.object_under_pointer() ~= c then
-                local geometry = c:geometry()
-                local x = geometry.x + geometry.width/2
-                local y = geometry.y + geometry.height/2
-                mouse.coords({x = x, y = y}, true)
-            end
-        end
-    })
-end
 
 client.connect_signal("manage", function (c)
     if awesome.startup
@@ -32,8 +15,5 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color   = beautiful.border_color_active end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_color_normal end)
-
-client.connect_signal("focus", move_mouse_onto_focused_client)
-client.connect_signal("swapped", move_mouse_onto_focused_client)
 
 screen.connect_signal("property::geometry", require "screens".set_wallpaper)
