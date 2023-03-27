cc46665061e6cfddc20b03a203cafcdcf19b92b8
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-03-27 09:29:30 -0300

    Added proper gtk and icons configuration

--- link.sh
+++ link.sh
@@ -2,13 +2,14 @@
 
 # links files to the ~
 
-mkdir ~/.config 2> /dev/null
-mkdir ~/.local 2> /dev/null
-mkdir ~/.local/share 2> /dev/null
+mkdir -p ~/.config 2> /dev/null
+mkdir -p ~/.local 2> /dev/null
+mkdir -p ~/.local/share 2> /dev/null
+mkdir -p ~/.icons/default 2> /dev/null
 ln -sf $PWD/.config/* ~/.config/
 ln -sf $PWD/.local/bin ~/.local/bin
 ln -sf $PWD/.local/share/* ~/.local/share
 ln -sf $PWD/.xprofile ~/.xprofile
 ln -sf $PWD/.zshenv ~/.zshenv
 ln -sf $PWD/.ghci ~/.ghci
-ln -sf $PWD/.icons ~/.icons
+ln -sf $PWD/.icons/default/index.theme ~/.icons/default/index.theme
