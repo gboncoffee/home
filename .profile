238949d1b3eed706f7e6d2da496cb1e7deb65e89
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-07-11 00:44:32 -0300

    General settings for simplify stuff
    I'm using KDE Plasma but I'll keep everything here

--- .profile
+++ .profile
@@ -14,48 +14,14 @@ if [ -n "$BASH_VERSION" ]; then
     fi
 fi
 
-#
-# moving configs away from ~
-#
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_CONFIG_HOME="$HOME/.config"
-export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0"
-export GNUPGHOME="$XDG_DATA_HOME/gnupg"
-export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"
-# shell and utils
-export HISTFILE="$XDG_DATA_HOME/bash/histfile"
-export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
-export LESSHISTFILE="-"
-# tex
-export TEXMFHOME="$XDG_DATA_HOME/tex/texmf"
-export TEXMFVAR="$XDG_DATA_HOME/tex/textmf-var"
-export TEXMFCONFIG="$XDG_CONFIG_HOME/tex/textmf-config"
-# js
-export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history"
-# rust
-export CARGO_HOME="$XDG_DATA_HOME/cargo"
-export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
-# python
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/pyrc.py"
-# go
-export GOPATH="$XDG_DATA_HOME/go"
-
-# default programs
 export EDITOR=nvim
 export BROWSER=chromium
 export TERMINAL=alacritty
 
-# fuck this language. Sun Microsystems and Oracle I FUCKING HATE YOU FOR
-# CREATING THE POST-MODERN CYBERPUNK REALITY. I FUCKING HATE YOU ALL!!!!!!!
-#
-# sorry Bill Joy, you're actually a cool guy ;)
-export _JAVA_AWT_WM_NONREPARENTING=1
 # this makes Java use GTK, system fonts and antialiasing
 export JDK_JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 
 # sensible defaults
-export GTK_USE_PORTAL=1
 export GTK_THEME=Dracula
-export QT_QPA_PLATFORMTHEME=qt5ct
-export QT_STYLE_OVERRIDE=kvantum
-export PATH="$HOME/.local/bin/:$HOME/opt/bin:$PATH:$HOME/.local/share/cargo/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
--- home.nix
+++ home.nix
@@ -20,19 +20,10 @@
     ocaml
     gh
     # not dev
-    feh
-    nnn
-    flameshot
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.qt5ct
-    lxqt.lxqt-policykit
-    pulsemixer
-    dunst
-    libnotify
     fd
     ripgrep
-    mpdscribble
-    pcmanfm-qt
+    mpd
+    cantata
   ];
 
   home.file = {
@@ -40,37 +31,16 @@
     ".ghci".source = ./.ghci;
     ".profile".source = ./.profile;
     ".xprofile".source = ./.xprofile;
-    ".local/bin/dmenu_bluetooth".source = ./.local/bin/dmenu_bluetooth;
-    ".local/bin/dmenu_shutdown".source = ./.local/bin/dmenu_shutdown;
-    ".local/bin/dmenu_web".source = ./.local/bin/dmenu_web;
-    ".local/bin/luabatmon".source = ./.local/bin/luabatmon;
-    ".local/bin/monitors".source = ./.local/bin/monitors;
-    ".local/bin/setxbg".source = ./.local/bin/setxbg;
-    ".local/bin/nnn".source = ./.local/bin/nnn;
-    ".config/bookmarks".source = ./.config/bookmarks;
-    ".config/chromium-flags.conf".source = ./.config/chromium-flags.conf;
     ".config/pyrc.py".source = ./.config/pyrc.py;
-    ".config/user-dirs.locale".source = ./.config/user-dirs.locale;
-    ".config/user-dirs.dirs".source = ./.config/user-dirs.dirs;
-    ".config/alacritty/alacritty.yml".source = ./.config/alacritty/alacritty.yml;
-    ".config/cava/config".source = ./.config/cava/config;
-    ".config/dunst/dunstrc".source = ./.config/dunst/dunstrc;
     ".config/git/config".source = ./.config/git/config;
     ".config/htop/htoprc".source = ./.config/htop/htoprc;
-    ".config/mpd/mpd.conf".source = ./.config/mpd/mpd.conf;
-    ".config/ncmpcpp/config".source = ./.config/ncmpcpp/config;
-    ".config/ncmpcpp/bindings".source = ./.config/ncmpcpp/bindings;
-    ".config/picom/picom.conf".source = ./.config/picom/picom.conf;
-    ".config/polybar/config.ini".source = ./.config/polybar/config.ini;
     ".config/tmux/tmux.conf".source = ./.config/tmux/tmux.conf;
-    ".config/xmonad/xmonad.hs".source = ./.config/xmonad/xmonad.hs;
   };
 
   xresources.properties = {
     "Xcursor.theme" = "Dracula-cursors";
   };
 
-  qt.platformTheme = "qt5ct";
   programs.home-manager.enable = true;
   home.stateVersion = "23.05"; # DONT'T CHANGE THIS MANUALLY!
 }
